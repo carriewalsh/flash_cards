b78912c280760c32a117447f2273327eaf53288d
Carrie Walsh <carriewalsh@gmail.com>
2019-01-26 09:50:16 -0700

    Adds Iteration 4 first attempts using CSV

--- flashcard_runner.rb
+++ flashcard_runner.rb
@@ -3,14 +3,20 @@ require "./lib/card"
 require "./lib/deck"
 require "./lib/round"
 require "./lib/card_generator"
-require "cards.txt"
+# require "./cards.txt"
 require "pry"
 
 
-
-cards = CardGenerator.new("cards.txt").cards
+# card1 = Card.new("What is the first element on the periodic table?", "hydrogen", ["hidrogen"], :chemistry)
+# card2 = Card.new("What is Newton's first law?", "an object at rest stays at rest", ["inertia"], :physics)
+# card3 = Card.new("What is the Latin name for a rat?", "Rattus rattus", ["rattus rattus"], :biology)
+# card4 = Card.new("What is the most diverse animal phylum?", "arthropoda", ["arthropods","insects"],:biology)
+# card5 = Card.new("What symbol is equal to the square root of -1?", "i", ["I"],:math)
+cards = CardGenerator.new("cards.txt")
+cards.create_cards
 deck = Deck.new
 deck << cards
+
 deck.array_of_categories
 round = Round.new(deck)
 
--- lib/card_generator.rb
+++ lib/card_generator.rb
@@ -1,19 +1,30 @@
-require "./cards.txt"
+# require "./cards.txt"
+require "./lib/card"
+require "csv"
+require "pry"
+
 filename = "cards.txt"
 
+
 class CardGenerator
-  attr_reader :filename
+  attr_reader :filename, :cards
 
   def initialize(filename)
     @filename = filename
     @cards = []
   end
 
-  def cards
-    File.open(@filename).each do |line|
-      @cards << line
+  def create_cards
+    contents = CSV.open filename, headers:true, header_converters: :symbol
+    contents.each do |row|
+      card = row[:card]
+      question = row[:question]
+      acceptable_answers = row[:acceptable_answers]
+      half_answers = row[:half_answers]
+      category = [:category]
+      card = Card.new(question,acceptable_answers,half_answers,category)
+      @cards << card
     end
-    return @cards
   end
 end
 
